955bcea7a42c9800feee351f924ab99ec25d28a7
UpsidedownFurnace <muffinshadez@gmail.com>
2024-08-16 07:02:37 -0400

    Add files via upload

--- dat.js
+++ dat.js
@@ -9,6 +9,38 @@ const map_parts = [
     }
 ];
 
-const pois = [
-    
+const location_dat = [
+    {
+        x: 0,
+        y: 0,
+        uname: 'test'
+    }
 ];
+
+const pois = [
+    {
+        x: -128,
+        y: 81,
+        name: 'West Burnin',
+    },
+    {
+        x: -6,
+        y: -119,
+        name: 'East Burnin'
+    },
+    {
+        x: -296,
+        y: -184,
+        name: 'Forgehelm'
+    },
+    {
+        x: -424,
+        y: -228,
+        name: 'Wiiland'
+    },
+    {
+        x: -21,
+        y: -233,
+        name: 'Glacia'
+    }
+]
--- map.js
+++ map.js
@@ -1,8 +1,99 @@
 (function() {
+    //for encoding / decoding level data as base64
+var b64 = {
+    table: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_+',
+    bitCombine: function(bits, i, len) {
+      var r = 0;
+      for (var j = 0; j < len; j++) { //basically just collects all the bits into a int / long
+        r |= bits[i++];
+        r <<= 1;
+      }
+      return r >> 1;
+    },
+    encode: function(dat) {
+      var res = "";
+      
+      var bits = [],v = null;
+      
+      //extract all the bits from the bytes to encode
+      for (var i = 0; i < dat.length; i++) { //cant use of :/
+        v = dat[i];
+        for (var b = 0; b < 8; b++) {
+          bits.push(
+            (v >> b) 
+            & 1
+          );
+        }
+      }
+      
+      //combine the bits into 6 bit chunks from 0-64 and index a table to get the character
+      for (i = 0; i < bits.length; i += 6) {
+        var cmax = i >= bits.length ? 6 - (i - bits.length) : 6;
+        res += this.table[
+          this.bitCombine(bits, i, cmax)
+        ];
+      }
+      return res;
+    },
+    decode: function(dat) {
+      var bits = [];
+      var v;
+      
+      //collect all the bytes
+      for (var i = 0; i < dat.length; i ++) {
+        v = this.table.indexOf(dat[i]); //opposite of the prior table indexing
+        for (var b = 0; b < 6; b++)
+          bits.push(
+            (v >> (5-b)) & 1  
+          );
+      }
+      
+      var res = [];
+      
+      //now assemble as bytes instead of 6 bit chunks
+      var val = 0;
+      for (var b = 0; b < bits.length; b += 8) {
+        val = 0;
+        var cmax = b >= bits.length ? 8 - (b - bits.length) : 8;
+        for (var j = 0; j < cmax; j++) {
+          val |= (bits[b+j] << j);
+        }
+        res.push(val);
+      }
+      
+      return res;
+    }
+  };
+  
+  //Binary utility functions
+  var bin = {
+    combineBytes: function(bytes, i, sz) {
+      var r = 0;
+      
+      for (var b = i+sz-1; b >= i;) {
+        r |= bytes[b--];
+        r <<= 8;
+      }
+      
+      return r >> 8;
+    },
+    intSplit: function(v, sz) {
+      var r = [];
+      
+      for (var i = 0; i < sz; i++) {
+        r.push(v & 255);
+        v >>= 8;
+      }
+      
+      return r;
+    }
+  };
+
     const MAP_W = 10000, MAP_H = 10000;
     const zoomMin = 0.15, zoomMax = 10;
     const can = document.querySelector('#c');
     const cPosDisplay = document.querySelector('.cursor-pos');
+    const pointRad = 7, hPointRad = pointRad * 0.5;
 
     const settings = {
         spg: document.querySelector('#spg')
@@ -60,19 +151,21 @@
         var i = 0;
 
         for (var p of points) {
+            if (i == 0) {
+                ctx.moveTo(p[0] + x, p[1] + y);
+            } else {
+                ctx.lineTo(p[0] + x, p[1] + y);
+              }
             i++;
-            if (i == 0)
-                ctx.moveTo(p.x + x, p.y + y);
-            else
-                ctx.lineTo(p.x + x, p.y + y);
         }
+        ctx.closePath();
     }
 
     function renderLocationPoint(x, y, hover, dat) {
         ctx.lineWidth = 3;
         ctx.strokeStyle = '#000';
         ctx.beginPath();
-        ctx.arc(x, y, 5, 0, Math.PI * 2, false);
+        ctx.arc(x, y, pointRad, 0, Math.PI * 2, false);
         ctx.stroke();
         ctx.fill();
 
@@ -80,22 +173,33 @@
             //TODO: display info above name
             const triPoints = [
                 [0,0],
-                [5,0],
-                [2.5, 5]
+                [10,0],
+                [5, 10]
             ];
 
-            var name_len = ctx.measureText(dat.uname || '').width;
-            const h = 20;
             ctx.font = '15px Arial';
+            var name_len = ctx.measureText(dat.uname || '').width;
+            const h = 22;
             const sPadding = 5, ttop = 2;
             name_len += sPadding * 2;
-            const aDist = 3; //distance above point
+            const aDist = 7; //distance above point
 
             ctx.fillStyle = '#fff';
-            const _x = x - name_len / 2, _y = y - (h + aDist + 5);
-            ctx.fillRect(_x, _y, name_len, h);
-            polyDraw(_x + (name_len / 2 - 2.5), _y + h, triPoints);
+            const hnl = name_len * 0.5;
+            const _x = x - hnl - hPointRad, _y = y - (h + aDist + 12 + pointRad);
+            ctx.fillRect(_x, _y + 1, name_len, h);
+            ctx.lineWidth = 0;
+            polyDraw(_x + (hnl - hPointRad), _y + h, triPoints);
             ctx.fill();
+            ctx.fillStyle = '#000';
+            ctx.fillText(dat.uname || '', _x + sPadding, _y + ttop + h / 2 + 2);
+        }
+    }
+
+    function translateCoord(x, y) {
+        return {
+            x: (((x) * zoom - origin.x) + can.width / 2),
+            y: (((y) * zoom - origin.y) + can.height / 2)
         }
     }
 
@@ -130,10 +234,40 @@
         if (location.x != null && location.y != null) {
             ctx.strokeStyle = '#000';
             ctx.fillStyle = '#f00';
+            const loc = translateCoord(location.x - hPointRad, location.y - hPointRad);
             renderLocationPoint(
-                ((location.x * zoom - origin.x) + can.width / 2), 
-                ((location.y * zoom - origin.y) + can.height / 2), true, {uname:'bob'}
+                loc.x, 
+                loc.y, 
+                Math.sqrt((mx - loc.x) ** 2 + (my - loc.y) ** 2) < pointRad, 
+                {uname:'u/'+location.uname}
             );
+        }
+
+        //render other locations
+        for (const up of location_dat) {
+            const nLoc = translateCoord(up.x - 2.5, up.y - 2.5),
+                  hov = Math.sqrt((mx - nLoc.x) ** 2 + (my - nLoc.y) ** 2) < 5;
+
+            ctx.fillStyle = hov ? '#090' : '#0f0';
+
+            renderLocationPoint(
+                nLoc.x, 
+                nLoc.y, 
+                hov, 
+                {uname:'u/'+up.uname}
+            );
+        }
+
+        //render pois
+        for (const poi of pois) {
+            const name = poi.name || '???';
+            ctx.font = '25px Arial';
+            ctx.fillStyle = '#000';
+            ctx.strokeStyle = '#fff';
+            ctx.lineWidth = 3;
+            const tw1 = ctx.measureText(name).width, pos = translateCoord(poi.x - tw1 * 0.5, poi.y);
+            ctx.strokeText(name, pos.x, pos.y);
+            ctx.fillText(name, pos.x, pos.y);
         }
 
         //draw global grid thingy
@@ -250,5 +384,6 @@
         alert('Click on a point where you want to set your location.');
         can.style.cursor = 'pointer';
         update_loc = true;
+        location.uname = name;
     })
 })();
