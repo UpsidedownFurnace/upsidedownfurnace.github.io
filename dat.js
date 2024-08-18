a535baa81d29125db1eb86284b2c91fa75fe3e61
UpsidedownFurnace <muffinshadez@gmail.com>
2024-08-18 11:24:39 -0400

    Switched to new map

--- dat.js
+++ dat.js
@@ -1,5 +1,5 @@
 const map_parts = [
-    {
+    /*{
         path: "map/mapmain.png",
         origin: {
             x: 0,
@@ -46,281 +46,314 @@ const map_parts = [
             y: 60
         },
         scale: 1
-    },
+    },*/
+    {
+        path: 'map/WORLD MAP.jpg',
+        origin: {
+            x: 0,
+            y: 0
+        },
+        scale: 1
+    }
 ];
 
 const location_dat = [
     {
-        x: 70,
-        y: 17,
+        x: -18,
+        y: -929,
         uname: 'furnacejesus'
     },
     {
-        x: 53,
-        y: -28,
+        x: 201,
+        y: -490,
         uname: 'ilovefurnaces'
     },
     {
-        x: -10,
-        y: -97,
+        x: 357,
+        y: -217,
         uname: 'furnacemarx'
     },
     {
-        x: 109,
-        y: 119,
+        x: 98,
+        y: -660,
         uname: 'comradefurnace',
         leader:true
     },
     {
-        x: -21,
-        y: 60,
+        x: -26,
+        y: -330,
         uname: 'F.P.D',
         nou: true
     },
     {
-        x: -126,
-        y: 26,
+        x: -412,
+        y: -864,
         uname: 'capitalistfurnace'
     },
     {
-        x: -122,
-        y: -105,
+        x: -451,
+        y: -630,
         uname: 'invertedfurnace'
     },
     {
-        x: -195,
-        y: -34,
+        x: -412,
+        y: -434,
         uname: 'americanfurnace'
     },
     {
-        x: -330,
-        y: -108,
+        x: -1212,
+        y: -1122,
         uname: 'anvilleader'
     },
     {
-        x: -470,
-        y: -109,
+        x: -1887,
+        y: -1013,
         uname: 'i_eat_wii'
     },
     {
-        x: -464,
-        y: -183,
+        x: -1727,
+        y: -1365,
         uname: 'i_like_wii'
     },
     {
-        x: -361,
-        y: -263,
+        x: -1449,
+        y: -1554,
         uname: 'eminem_wii'
     },
     {
-        x: -89,
-        y: -221,
+        x: -400,
+        y: -1474,
         uname: 'freezerpresident',
         leader:true
     },
     {
-        x: 64,
-        y: -174,
+        x: 151,
+        y: -1400,
         uname: 'fakefurnace'
     },
     {
-        x: 84,
-        y: -108,
+        x: 308,
+        y: -996,
         uname: 'comicalylargefreezer'
     },
     {
-        x: 222,
-        y: -64,
+        x: 1112,
+        y: -89,
         uname: 'furnacekiller'
     },
     {
-        x: 199,
-        y: 35,
+        x: 1241,
+        y: -476,
         uname: 'ikillfurnacesforfun'
     },
     {
-        x: 348,
-        y: 4,
+        x: 990,
+        y: -705,
         uname: 'antifurnacegroup'
     },
     {
-        x: 380,
-        y: -92,
+        x: 1445,
+        y: -948,
         uname: 'femboyfurnace'
     },
     {
-        x: 197,
-        y: -146,
+        x: 710,
+        y: -1137,
         uname: 'kawaiifurnace',
         leader:true
     },
     {
-        x: 201,
-        y: 260,
+        x: 748,
+        y: 335,
         uname: 'handsomefurnace'
     },
     {
-        x: -56,
-        y: 382,
+        x: -265,
+        y: 853,
         uname: 'ThaiFurnace',
         nou: true
     },
     {
-        x: -167,
-        y: 376,
+        x: -630,
+        y: 870,
         uname: 'iFurnace',
         nou: true
     },
     {
-        x: -90,
-        y: 120,
+        x: -229,
+        y: -140,
         uname: 'iunfuckfurnaces'
     },
     {
-        x: 226,
-        y: 127,
+        x: 1002,
+        y: -406,
         uname: 'iwillendfurnaces'
     },
     {
-        x: 135,
-        y: -200,
+        x: 496,
+        y: -1204,
         uname: 'metalfurnace'
     },
     {
-        x: -298,
-        y: 114,
+        x: -1057,
+        y: -239,
         uname: 'dragonfurnace'
     },
     {
-        x: 96,
-        y: -309,
+        x: 355,
+        y: -1669,
         uname: 'freezersorcerer'
     },
     {
-        x: -599,
-        y: 326,
+        x: -1746,
+        y: 746,
         uname: 'eldritchfurnace'
     },
     {
-        x: -824,
-        y: 85,
+        x: -1600,
+        y: 345,
         uname: 'upsidedownfurnace',
         leader:true
     },
     {
-        x: 414,
-        y: -48,
+        x: 1614,
+        y: -497,
         uname: 'furnacekitty'
     },
     {
-        x:-266,
-        y:-476,
-        g:"idownloadfurnaces"
+        x:-1000,
+        y:-2276,
+        uname:"idownloadfurnaces"
     }
 ];
 
 const pois = [
     {
-        x: -128,
-        y: 81,
+        x: -500,
+        y: -300,
         name: 'West Burnin',
     },
     {
-        x: 22,
-        y: -50,
+        x: 4,
+        y: -783,
         name: 'East Burnin'
     },
     {
-        x: -296,
-        y: -184,
+        x: -1213,
+        y: -1248,
         name: 'Forgehelm'
     },
     {
-        x: -424,
-        y: -228,
+        x: -1626,
+        y: -1434,
         name: 'Wiiland'
     },
     {
-        x: -21,
-        y: -233,
+        x: -263,
+        y: -1423,
         name: 'Glacia'
     },{
-        x: 10,
-        y: -332,
+        x: 25,
+        y: -1768,
         name: 'Coald Mines'
     },
     {
-        x: 282,
-        y: 7,
+        x: 983,
+        y: -580,
         name: 'Pyraxis'
     },
     {
-        x: 202,
-        y: -174,
+        x: 575,
+        y: -1250,
         name: 'Ignicis'
     },
     {
-        x: 0,
-        y: -480,
-        name: 'Unclaimed Territories'
+        x: -314,
+        y: -2391,
+        name: 'Cryos'
     },
     {
-        x: -595,
-        y: 298,
+        x: -1776,
+        y: 707,
         name: 'Eldritch Tower'
     },
     {
-        x: -834,
-        y: 49,
+        x: -1600,
+        y: 283,
         name: 'Furnacestralia'
     },
     {
-        x: -10000,
-        y: -10000,
-        name: 'Nothing here...'
+        x: -492,
+        y: 840,
+        name: 'F.F.F'
     },
     {
-        x: -20000,
-        y: -20000,
-        name: 'Try to guess my main'
+        x: 559,
+        y: 459,
+        name: 'Handsome Island'
     },
     {
-        x: -30000,
-        y: -30000,
-        name: 'Have a guess?'
+        x: 2177,
+        y: 1159,
+        name: 'South Arcania'
     },
     {
-        x: -40000,
-        y: -40000,
-        name: 'ok.'
+        x: 3297,
+        y: 13,
+        name: 'Calypsia'
     },
     {
-        x: -50000,
-        y: -50000,
-        name: 'it is...'
+        x: 4624,
+        y: 840,
+        name: 'Narvara'
+    },
+    {
+        x: 3982,
+        y: -1702,
+        name: 'North Arcania'
+    },
+    {
+        x: -3921,
+        y: 38,
+        name: 'Avalia'
+    },
+    {
+        x: -3856,
+        y: 960,
+        name: 'Furnia'
+    },
+    {
+        x: -413,
+        y: 2714,
+        name: "Ice Spire"
     }
 ]
 
 const towns = [
     {
-        x: -799,
-        y: 44,
+        x: -1583,
+        y: 328,
         name: 'New Furnace',
         population: 1,
         capital: true
     },
     {
-        x: -538,
-        y: -281,
+        x: -2019,
+        y: -1666,
         name: 'West Camp 1',
         population: 1
     },
     {
-        x: -265,
-        y: -472,
+        x: -1037,
+        y: -2275,
         name: 'Download Village',
         population: 1
-    }
+    },{
+        x: -106,
+        y: 70,
+        name: "Ofen Point",
+        population: 10
+    },
 ];
 
 const terrainDat = [
@@ -332,16 +365,63 @@ const terrainDat = [
         rot: 60
     },
     {
-        x: -411,
-        y: -700,
+        x: 44,
+        y: -2880,
         name: 'Freezepoint Sea',
-        size: 14,
-        rot: -15
+        size: 40,
+        rot: 15
+    },
+    {
+        x: 130,
+        y: 1549,
+        name: 'I R O N W A V E  O C E A N',
+        size: 180,
+        rot: 0
+    },
+    {
+        x: 4187,
+        y: 2363,
+        name: 'COLOSSIC OCEAN',
+        size: 100,
+        rot: 0
+    },
+    {
+        x: -5110,
+        y: 2002,
+        name: 'COLOSSIC OCEAN',
+        size: 100,
+        rot: 0
+    },
+    {
+        x: 2657,
+        y: -1509,
+        name: 'VORTIC OCEAN',
+        size: 120,
+        rot: 0
+    },
+    {
+        x: -3222,
+        y: -2577,
+        name: 'STORMCREST OCEAN',
+        size: 120,
+        rot: 0
     },
     {
         x: -245,
         y: -482,
         name: 'Charcoal Lake',
         size:7
+    },
+    {
+        x: -3335,
+        y: -2166,
+        name: '"Fuck you" Island',
+        size:20
+    },
+    {
+        x: 3915,
+        y: -739,
+        name: 'Calypsian Sea',
+        size:30
     }
-]
+];
